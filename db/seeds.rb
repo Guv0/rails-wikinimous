d12633f3bd0a4e6989f3103d05206cacf7e9fd94
Govinder Marwaha <govinder.marwaha@gmail.com>
2016-11-08 16:19:56 +0000

    Basic Wikinimiou completes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+articles = []
+
+10.times do
+  hsh = {
+  title: Faker::Book.title,
+  content: Faker::Lorem.paragraph,
+  }
+  articles << hsh
+end
+
+Article.create(articles)
+
+
